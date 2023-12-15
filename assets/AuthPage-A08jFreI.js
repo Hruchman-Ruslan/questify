c23234471c2b3dc1b9dea230bd937770aaf22a5a
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-12-15 18:01:26 +0000

    deploy: b0942df43d534fa43b5dc15393e2ebe6b1fbec99

---
+++ assets/AuthPage-A08jFreI.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-jCLcKq7u.js";const s="_title_g5sw8_1",e={title:s},i=()=>t.jsx("div",{children:t.jsx("h1",{className:e.title,children:"Questify"})}),n=()=>t.jsx(i,{});export{n as default};
--- assets/AuthPage-eQ5HC178.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-xLeqy0Rt.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"Auth Page"})}),r=()=>t.jsx(s,{});export{r as default};
