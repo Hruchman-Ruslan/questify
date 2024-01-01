131b7a8cc7a7f5a1ff3b982669512897c93b4341
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2024-01-01 20:48:04 +0000

    deploy: b65891fa1cc7aee38c9d361412dc199dfc0d8afe

--- assets/MainPage-FufpSr9y.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-W5ls2knx.js";const t=()=>e.jsx("div",{children:e.jsx("h2",{style:{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"},children:"Main Page"})}),s=()=>e.jsx(t,{});export{s as default};
